cb53143d207e070bb17981fecfa848f65cdbd817
Jang-Jungi <jjgweb951101@naver.com>
2021-05-06 14:54:16 +0900

    배열 종료

---
+++ OOPSolution/ArrayClassTestApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ArrayClassTestApp //p.365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array Class 사용");
+
+            int[] array = new int[] {5,7,3,1,9,10,4,8,2,6 };
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+
+            Array.Sort(array); // sort -> 오름차순 정렬
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+            
+            Array.Reverse(array); // Reverse -> 내림차순 정렬
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+
+            int index7 = Array.IndexOf(array,7);  // 배열 중에 7의 위치를 찾기
+            Console.WriteLine($"7의 위치는 {index7}");
+
+            Array.Resize<int>(ref array,11); // 11번째 수를 추가하기
+            array[10] = 100;
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("\n");
+
+            //P.372 2차원 배열
+            int[,] array1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array1.GetLength(0); j++)
+                {
+                    Console.Write($"[{i},{j}] : {array1[i,j]}\t\n");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- OOPSolution/ArrayTestApp/Program.cs
+++ OOPSolution/ArrayTestApp/Program.cs
@@ -8,13 +8,17 @@ namespace ArrayTestApp
         {
             Console.WriteLine("대성마이맥 수학점수");
 
-            int[] scores = new int[5];
+            int[] scores = new int[6];
             scores[0] = 80;
             scores[1] = 74;
             scores[2] = 81;
             scores[3] = 94;
             scores[4] = 100;
             //추가로 데이터를 추가하고 싶을 때는 용량값과, 점수값을 직접 입력해줘야한다.
+
+            int[] scores2 = new int[5] { 80, 74, 81, 91, 100 }; // 위의 내용을 초기화하는 방법 1
+
+            int[] scores3 = { 80, 74, 81, 91, 100 }; // 위의 내용을 최종으로 제일 짧게 초기화하는 방법 2
 
             // 점수 총합
             int sum = 0;
@@ -24,7 +28,7 @@ namespace ArrayTestApp
             }
 
             foreach (var item in scores) // 단순 반복문일 때는 foreach구문이 더 간단하다.
-            {
+            { 
                 sum += item;
             }
 
