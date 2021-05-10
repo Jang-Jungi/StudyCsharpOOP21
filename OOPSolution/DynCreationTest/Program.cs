15aaec7fa27b1e26b683f854955ef246c39f4e9c
Jang-Jungi <jjgweb951101@naver.com>
2021-05-10 12:46:54 +0900

    리플렉션

---
+++ OOPSolution/DynCreationTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynCreationTest
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public void Print() { Console.WriteLine($"{Name} : {PhoneNumber}"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile profile1 = new();
+            profile1.Name = "박찬호";
+            profile1.PhoneNumber = "010-2424-2424";
+            profile1.Print();
+
+            //리플렉션 객체생성
+            Type TYPE = typeof(Profile);
+            Object profile2 = Activator.CreateInstance(TYPE);
+            PropertyInfo name = TYPE.GetProperty("Name");
+            PropertyInfo phonenumber = TYPE.GetProperty("PhoneNumber");
+            name.SetValue(profile2, "류현진", null);
+            phonenumber.SetValue(profile2, "010-1111-1111", null);
+
+            MethodInfo print = TYPE.GetMethod("Print");
+            print.Invoke(profile2, null);
+            //엄청 복잡해서 잘 사용하지 않음
+        }
+    }
+}
---
+++ OOPSolution/ReflectionTestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionTestApp
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public void PrintProfile() { Console.WriteLine($"{Age},{Name}"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Type type = a.GetType(); // int32
+            Console.WriteLine("Field 타입 리스트: ");
+
+            FieldInfo[] fields = type.GetFields();
+            foreach (var item in fields)
+            {
+                Console.WriteLine($"{item.FieldType.Name}, Name : {item.Name}");
+            }
+            Console.WriteLine("Property 타입 리스트: ");
+
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (var item in properties)
+            {
+                Console.WriteLine($"{item.PropertyType.Name}, Name : {item.Name}");
+            }
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach (var item in methods)
+            {
+                Console.WriteLine($"{item.ReturnType.Name}, Name : {item.Name}");
+            }
+        }
+    }
+}
